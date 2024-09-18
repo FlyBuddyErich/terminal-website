a664de9570a2fb68a41d8c21a6d283952517ce87
FlyBuddyErich <maks10082004@gmail.com>
2024-09-18 11:21:41 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
+import App from './App';
+
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <App />
-  </>
-)
+  </React.StrictMode>
+);
+
